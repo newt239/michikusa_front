509691f45a8ddaae6c553b2cdd4c15ddc705b4d5
newt <ysti2019.win2020.st@gmail.com>
2024-08-01 12:18:33 +0900

    add: package of leaflet types

--- src/routes/map.tsx
+++ src/routes/map.tsx
@@ -1,5 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/map')({
-  component: () => <div>Hello /map!</div>
-})
+import { MapContainer, TileLayer } from "react-leaflet";
+
+export const Route = createFileRoute("/map")({
+  component: () => (
+    <div>
+      <MapContainer center={[51.505, -0.09]} scrollWheelZoom={false} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  ),
+});
