f15e2b3dce1f7044b1d31c6471feeb5141ae2f37
newt <ysti2019.win2020.st@gmail.com>
2024-08-03 13:00:12 +0900

    chore: restore WayPoints component

--- src/routes/map.tsx
+++ src/routes/map.tsx
@@ -4,6 +4,7 @@ import { Box } from "@yamada-ui/react";
 
 import HalfModal from "#/components/map/HalfModal";
 import Map from "#/components/map/Map";
+import WayPoints from "#/components/map/WayPoints";
 import "leaflet/dist/leaflet.css";
 
 export const Route = createFileRoute("/map")({
@@ -60,7 +61,9 @@ export const Route = createFileRoute("/map")({
     return (
       <Box h="full" w="full">
         <Map facilityList={facilityList} station={station} />
-        <Box left={0} pos="fixed" right={0} top={0} zIndex={1001}></Box>
+        <Box left={0} pos="fixed" right={0} top={0} zIndex={1001}>
+          <WayPoints />
+        </Box>
         <Box zIndex={1001}>
           <HalfModal facilityList={facilityList} station={station} />
         </Box>
